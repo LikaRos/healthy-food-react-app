8d6cf193d4e3590f5bad79a51aed306cef627f42
Lika <lika.rossinskaya@gmail.com>
2025-03-28 06:09:16 +0200

    environment added

--- src/ServiceApi/service.js
+++ src/ServiceApi/service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = `http://localhost:3001`;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 export const service = (page) => {
   return axios.get("/products", {
